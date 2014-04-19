ee572d6ee09ab8ec389e2cc6564fae23082a777d
boddie <jdboddie@live.com>
2014-04-19 15:48:08 -0500

    Added singleton class for project

---
+++ Powerhouse/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    // Creates an instance of itself
+    private void Awake()
+    {
+        if (Instance != null)
+            GameObject.Destroy(Instance);
+        else
+            Instance = this;
+        DontDestroyOnLoad(this);
+    }
+}
