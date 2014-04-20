d4764b24b9ee5fd58e40d3b24068f39581fda59b
boddie <jdboddie@live.com>
2014-04-20 03:50:52 -0500

    Scenes, Tools, Touch Controls, UI
    I added a game scene for play so the start screen now has just the start
    game option. I added a class GUITools that can draw progress bars for
    the UI. I also added a class for touch screen control and put it in
    scene (not yet implemented).

--- Powerhouse/Assets/Scripts/GameUI.cs
+++ Powerhouse/Assets/Scripts/GameUI.cs
@@ -5,16 +5,28 @@ public class GameUI : MonoBehaviour
 {
     private Texture _crosshair;
     private Rect _crosshairLoc;
+    private ScreenPad _move;
+    private ScreenPad _look;
 
-	void Start () 
+	private void Start () 
     {
         _crosshair = Resources.Load(@"Textures/crosshair") as Texture;
         _crosshairLoc = new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100);
         Screen.showCursor = false;
+        _move = new ScreenPad(new Rect(20, Screen.height - 370, 350, 350));
+        _look = new ScreenPad(new Rect(Screen.width - 370, Screen.height - 370, 350, 350));
 	}
 
-    void OnGUI()
+    private void Update()
+    {
+        _move.Update();
+        _look.Update();
+    }
+
+    private void OnGUI()
     {
         GUI.DrawTexture(_crosshairLoc, _crosshair);
+        _move.OnGUI();
+        _look.OnGUI();
     }
 }
--- Powerhouse/Assets/Scripts/MenuUI.cs
+++ Powerhouse/Assets/Scripts/MenuUI.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MenuUI : MonoBehaviour {
+public class MenuUI : MonoBehaviour 
+{
+    private Texture _title;
+    private Rect _titleRect;
+    public  GUISkin _style;
+    private Rect _startRect;
 
-	// Use this for initialization
-	void Start () {
-	
+	private void Start () 
+    {
+        _title = Resources.Load<Texture>(@"Textures/title");
+        _titleRect = new Rect(Screen.width / 2 - Screen.width * 0.75f / 2, 20, Screen.width * 0.75f, Screen.height * 0.3f);
+        _startRect = new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 70);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void OnGUI()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+        GUISkin current = GUI.skin;
+        GUI.skin = _style;
+        GUI.DrawTexture(_titleRect, _title);
+
+        Color prevColor = GUI.color;
+        if (_startRect.Contains(mousePos))
+        {
+            GUI.color = Color.gray;
+            GUI.Label(_startRect, "Start Game");
+            if (GUI.Button(_startRect, "", "Label"))
+                Application.LoadLevel("Game");
+        }
+        else
+        {
+            GUI.Label(_startRect, "Start Game");
+        }
+
+        GUI.color = prevColor;
+        GUI.skin = current;
+    }
 }
